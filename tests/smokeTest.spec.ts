3212700b1956404fc26a47de70245da0049820d6
Francisco Ramirez <vcity95@gmail.com>
2025-12-01 17:03:27 -0600

    Create Token function and implements in order to optimize execution performance.

--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -1,15 +1,11 @@
 import { test } from '../utils/fixtures';
 import { expect } from '../utils/custom-expect';
+import { createToken } from '../helpers/createToken';
 
 let authToken: string;
 
 test.beforeAll('run before all', async ({ api, config }) => {
-    const payload = { "user": { "email": config.userEmail, "password": config.userPassword } };
-    const tokenResponse = await api
-        .path('/users/login')
-        .body(payload)
-        .postRequest(200)
-    authToken = 'Token ' + tokenResponse.user.token;
+    authToken = await createToken(config.userEmail, config.userPassword);
 });
 
 test('Get Articles', async ({ api }) => {
